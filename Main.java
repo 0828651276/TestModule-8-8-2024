6c1cd426e1bd9195f059322934a9a2e7ab49f6ca
Le Van Chuyen <duythi794@gmail.com>
2024-08-08 09:44:08 +0700

    new

--- Main.java
+++ Main.java
@@ -8,8 +8,11 @@ public class Main {
         FictionBook fb2 = new FictionBook("FB002", "Fiction Two", 150, "Author E", "Horror");
         FictionBook fb3 = new FictionBook("FB003", "Fiction Three", 180, "Author F", "Sci-Fi");
 
-        double totalAmount = pb1.getAmount() + pb2.getAmount() + pb3.getAmount()
-                + fb1.getAmount() + fb2.getAmount() + fb3.getAmount();
+        Book[] books = {pb1, pb2, pb3, fb1, fb2, fb3};
+        double totalAmount = 0;
+        for (Book book : books) {
+            totalAmount += book.getPrice() * book.getAmount();
+        }
 
         System.out.println("Total amount for all books: " + totalAmount);
 
@@ -22,5 +25,23 @@ public class Main {
         }
 
         System.out.println("Number of ProgrammingBooks with language 'Java': " + javaBookCount);
+
+        int FictionCount = 0;
+        FictionBook[] fictionBooks = {fb1, fb2, fb3};
+        for (FictionBook book : fictionBooks) {
+            if ("Fiction One".equalsIgnoreCase(book.getName())) {
+                FictionCount++;
+            }
+        }
+
+        System.out.println("Number of Fictions: " + FictionCount);
+
+        int FictionPrice = 0;
+        for (FictionBook book : fictionBooks) {
+            if (book.getPrice() >100){
+                FictionPrice++;
+            }
+        }
+        System.out.println("Number > 100:" +FictionPrice);
     }
 }
